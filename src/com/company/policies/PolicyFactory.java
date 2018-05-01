2e193659d123e4497f77866da7ba9a79e021e40e
William Hexberg <whexberg@gmail.com>
2018-04-30 21:34:34 -0700

    Allow lowercase algorithm name

--- src/com/company/policies/PolicyFactory.java
+++ src/com/company/policies/PolicyFactory.java
@@ -2,11 +2,11 @@ package com.company.policies;
 
 public class PolicyFactory {
     public static PagePolicy getPagePolicy(String policyType, int numPages, int numFrames, int numRequests) {
-        if (policyType.equals("FIFO"))
+        if (policyType.toUpperCase().equals("FIFO"))
             return new FifoPolicy(numPages, numFrames, numRequests);
-        else if (policyType.equals("LRU"))
+        else if (policyType.toUpperCase().equals("LRU"))
             return new LruPolicy(numPages, numFrames, numRequests);
-        else if (policyType.equals("OPT"))
+        else if (policyType.toUpperCase().equals("OPT"))
             return new OptPolicy(numPages, numFrames, numRequests);
         else
             throw new IllegalArgumentException("Allowed algorithms: FIFO, LRU, and OPT");
